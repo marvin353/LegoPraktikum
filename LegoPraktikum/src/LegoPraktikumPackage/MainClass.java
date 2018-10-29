56696a9d184bc29eae27e7194b632b874a082499
jessimply <uxebf@student.kit.edu>
2018-10-29 09:05:16 +0100

    Main Method Kommentare

--- LegoPraktikum/src/LegoPraktikumPackage/MainClass.java
+++ LegoPraktikum/src/LegoPraktikumPackage/MainClass.java
@@ -4,6 +4,13 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		//Menu anzeigen
+		
+		//Je nach Auswahl entsprechende Section starten oder Rennen starten
+		
+		//Sensor Thread starten
 
 	}
 
