db23a6a91f838d222f467a2cb4d26ec1fe4bc47c
Lennard Sommer <lennard.sommer@gmx.de>
2018-11-05 10:41:40 +0100

    Obstcle fix

--- LegoPraktikum/src/States/LinieFahren.java
+++ LegoPraktikum/src/States/LinieFahren.java
@@ -1,6 +1,7 @@
 package States;
 
 import lejos.utility.Delay;
+import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
@@ -106,7 +107,8 @@ public class LinieFahren implements Runnable, ISection {
     	  int initTachoCountLeft = robot.getTachoCountLeftMotor();
         int initTachoCountRight = robot.getTachoCountRightMotor();
         int deltaLeft = 0, deltaRight = 0;
-        while(deltaLeft <= 360 || deltaRight <= 360) {
+        LCD.drawString("Zurück", 0, 5);
+        while(Math.abs(deltaLeft) <= 360 || Math.abs(deltaRight) <= 360) {
           robot.stopLeftMotor();
           robot.setRightMotorSpeed(100);
           
@@ -119,9 +121,10 @@ public class LinieFahren implements Runnable, ISection {
         initTachoCountRight = robot.getTachoCountRightMotor();
         deltaLeft = 0;
         deltaRight = 0;
-        while(deltaLeft <= 720 || deltaRight <= 720) {
-          robot.setLeftMotorSpeed(100);
-          robot.setRightMotorSpeed(100);
+        LCD.drawString("Vorwärts", 0, 5);
+        while(Math.abs(deltaLeft) <= 720 || Math.abs(deltaRight) <= 720) {
+          robot.setLeftMotorSpeed(200);
+          robot.setRightMotorSpeed(200);
           
           robot.setRightMotorGoForward();
           robot.setLeftMotorGoForward();
@@ -133,18 +136,20 @@ public class LinieFahren implements Runnable, ISection {
         initTachoCountRight = robot.getTachoCountRightMotor();
         deltaLeft = 0;
         deltaRight = 0;
-        while(deltaLeft <= 360 || deltaRight <= 360) {
+        LCD.drawString("Links drehen", 0, 5);
+        while(Math.abs(deltaLeft) <= 360 || Math.abs(deltaRight) <= 360) {
           robot.stopLeftMotor();
-          robot.setRightMotorSpeed(100);
+          robot.setRightMotorSpeed(200);
           
           robot.setRightMotorGoForward();
           
           deltaLeft = robot.getTachoCountLeftMotor() - initTachoCountLeft;
-          deltaRight = robot.getTachoCountRightMotor() - initTachoCountRight;
+          deltaRight = robot.getTachoCountRightM0tor() - initTachoCountRight;
         }
+        LCD.drawString("Linie suchen", 0, 5);
         while(brightness <= 0.1) {
-          robot.setLeftMotorSpeed(100);
-          robot.setRightMotorSpeed(100);
+          robot.setLeftMotorSpeed(200);
+          robot.setRightMotorSpeed(200);
           
           robot.setRightMotorGoForward();
           robot.setLeftMotorGoForward();
